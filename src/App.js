8ea54414a5dc05f8a55fd4b456da58e01ad818ad
SSB0734 <shailendrasinghbhati0734@gmail.com>
2020-03-28 18:56:25 +0530

    components part two

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
+import {DISHES} from './shared/dishes'
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component{
+constructor(props){
+  super(props);
+  this.state = {
+    dishes:DISHES
+  };
+}
+
   render(){
     return(
       <div>
@@ -15,7 +23,7 @@ class App extends Component{
       </NavbarBrand>
       </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
