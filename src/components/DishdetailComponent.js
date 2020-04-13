b9ba491212a5b3cb1326dba4a732830414a5b5f4
SSB0734 <shailendrasinghbhati0734@gmail.com>
2020-04-13 13:47:25 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import CommentForm from './CommentForm';
+import { Loading } from './LoadingComponent';
 
     function RenderDish({dish}){
 		if(dish!=null){
@@ -45,7 +46,25 @@ import CommentForm from './CommentForm';
     }
 
     const DishDetail = (props) =>{//or const DishDetail = function DishDetail(props){} or ... simply function DishDetail(props){}
-        if(props.dish!=null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null){
         return(
             <div className="container">
                 <div className="row">
